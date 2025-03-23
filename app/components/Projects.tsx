c63c146d114ed2930318b74f60ec21cba519a406
mobo07 <mikemobo900@gmail.com>
2025-03-23 20:58:10 +0100

    added word-seeker to featured projects

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,6 +2,7 @@
 import ProjectCard from "./ProjectCard";
 import Title from "./Title";
 import bespoken from "../assets/projects/bespoken.png";
+import wordseeker from "../assets/projects/word-seeker.png";
 import countries from "../assets/projects/countries-app.png";
 import burgerapp from "../assets/projects/burger-app.png";
 import todoapp from "../assets/projects/todo-app.png";
@@ -72,6 +73,15 @@ const Projects = () => {
         />
         <ProjectCard
           orientation="left"
+          title="WordSeeker"
+          desc="A web app that comes in handy when you find yourself in one of those moments whereby you know the definition of a word you want to use but can't really say what the word is. WordSeker leverages AI to accurately suggest the word you're looking for."
+          img={wordseeker}
+          stack={["Nextjs", "Tailwind CSS", "TypeScript", "Gemini API"]}
+          githubLink="https://github.com/mobo07/word-seeker"
+          liveURL="https://word-seeker.vercel.app"
+        />
+        <ProjectCard
+          orientation="right"
           title="Countries of the World"
           desc="A fun web application to help you learn about any country of the world including its relevant details such as region, capital, border countries, languages and more!..."
           img={countries}
@@ -80,7 +90,7 @@ const Projects = () => {
           liveURL="https://countries-app-nextjs.vercel.app"
         />
         <ProjectCard
-          orientation="right"
+          orientation="left"
           title="Analytics Dashboard"
           desc="A nice interactive dashboard with a sleek user interface and user experience that features an interactive chart, sortable table and light/dark mode toggle. Also fully responsive"
           img={dashboard}
@@ -89,7 +99,7 @@ const Projects = () => {
           liveURL="https://analytics-dashboard-ht7e.vercel.app"
         />
         <ProjectCard
-          orientation="left"
+          orientation="right"
           title="Burger App"
           desc="A web application that enables users to put together and place an order of a tasty burger just the way you like it by adding or removing ingredients of your choice."
           img={burgerapp}
@@ -98,7 +108,7 @@ const Projects = () => {
           liveURL="https://burger-app-brown.vercel.app"
         />
         <ProjectCard
-          orientation="right"
+          orientation="left"
           title="Todo App"
           desc="A beautiful todo web application to help keep track of your tasks."
           img={todoapp}
